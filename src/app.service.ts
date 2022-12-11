78d3308460cae9b0fe7abcd65477cccc99a9903d
Bo George <bo.george@transport4.com>
2022-12-11 11:26:39 -0500

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
-
+import { env } from 'node:process';
 @Injectable()
 export class AppService {
   getHello(): string {
+
+    console.log(env.ES_ID)
+
     return 'Hello World!!';
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { env } from 'node:process';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -11,8 +10,6 @@ describe('AppController (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    console.log(env.ES_ID)
 
     app = moduleFixture.createNestApplication();
     await app.init();
